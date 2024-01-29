3e01c747bf367cf468bac747ea450aad7cd43975
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-29 11:48:56 +0100

    Create orbis-moneybird.php

---
+++ orbis-moneybird.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Orbis Moneybird
+ *
+ * @package   Pronamic\Orbis\Moneybird
+ * @author    Pronamic
+ * @copyright 2024 Pronamic
+ * @license   GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Orbis Moneybird
+ * Plugin URI:        https://wp.pronamic.directory/plugins/orbis-moneybird/
+ * Description:       This WordPress plugin provides the link between Orbis and your Moneybird administration.
+ * Version:           1.0.0
+ * Requires at least: 6.2
+ * Requires PHP:      8.0
+ * Author:            Pronamic
+ * Author URI:        https://www.pronamic.eu/
+ * Text Domain:       orbis-moneybird
+ * Domain Path:       /languages/
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wp.pronamic.directory/plugins/orbis-moneybird/
+ * GitHub URI:        https://github.com/pronamic/orbis-moneybird
+ */
+
+add_action(
+	'init',
+	function () {
+	},
+	200
+);
